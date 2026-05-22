6bc1755fc04235e5f183c2591682a631bed68360
abdullahayaz-12 <abdullah.ayaz@techbridgeconsultancy.com>
2026-05-22 15:57:42 +0000

    update homepage text

--- src/components/Homeintro/Homeintro.jsx
+++ src/components/Homeintro/Homeintro.jsx
@@ -12,7 +12,7 @@ const Homeintro = (prop) => {
             <div className="row">
                 <div className="col-lg-7">
                      <h1>
-                        GET YOUR RANK <span>IN SWIFTBOOST</span>
+                        GET YOUR LEAGUE RANK <span>IN SWIFTBOOST</span>
                      </h1>
                      <div className="homeintrobtns">
                         <button className='startboostbtn'>
